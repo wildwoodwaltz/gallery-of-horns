a21f03bd29907388fc44addcf452b42632060e03
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-03-14 15:36:28 -0700

    added Footer with info

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        &copy; Tyler Egashira, 2022
+      </footer>
+    )
+  }
+}
+export default Footer;
