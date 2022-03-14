f3de84c8918d2d61aeb9d1a180707b8e2076622f
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-03-14 16:38:39 -0700

    created prop info to be retrieved

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,21 @@
 import React from "react";
 import HornedBeast from "./HornedBeast.js";
+import Narwhal from "./Narwhal.jpg"
+import Rhino from "./rhino.PNG"
+
 
 class Main extends React.Component {
   render() {
     return (
       <>
-      <HornedBeast image_url="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
-    title = "UniWhal" description = "A unicorn and a narwhal nuzzling their horns" keyword = "narwhal"/>
-      <HornedBeast/>
+      <HornedBeast src = {Narwhal}
+      title = "UniWhal" 
+      description = "A unicorn and a narwhal nuzzling their horns" keyword = "narwhal"/>
+      <HornedBeast src= {Rhino}
+      title = "Rhino Binos"
+      description = "Parent rhino with two babies"
+      keyword = "rhino"
+      />
       </>
     )
   }
