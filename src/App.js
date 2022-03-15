70704bda4448fe17f5ba682a92520ea6f4768914
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-03-15 15:32:51 -0700

    added data linking from data.json

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
 import './App.css';
+import data from './data.json';
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Header/>
-        <Main/>
+        <Main data={data}/>
         <Footer/>
       </>
     )
